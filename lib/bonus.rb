c522e1b1cc4f27379e9eb249c83e644dd37aaedc
kyle molidor <k_molidor@yahoo.com>
2020-02-23 22:29:50 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,8 +23,8 @@ def bonus
 
   #code your solution here:
 
-  epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:heroine][:status] = "deadl"
+  epic_tragedy[:montague][:hero][:status] = "Michael Jordan"
+  
  
 
 
